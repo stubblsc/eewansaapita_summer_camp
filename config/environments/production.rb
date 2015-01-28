fd5769b8da6b6a2be45eb2e070a3b980b5345313
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-01-28 12:27:23 -0500

    updated config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Eewansaapita::Application.configure do
   config.assets.compress = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
